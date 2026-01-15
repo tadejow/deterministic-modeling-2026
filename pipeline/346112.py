dea7c86174c06e48b3fec10a1cbefb2eeff00d8d
Jacek Kowalski <346112@uwr.edu.pl>
2026-01-15 13:48:32 +0100

    346112

--- pipeline/346112.py
+++ pipeline/346112.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def plot_function():
-	x = np.linspace(-1, 1, 100)	# tutaj uzupełnij
-	y = np.sin(x)							# tutaj uzupełnij
+	x = np.linspace(-10, 10, 100)	# tutaj uzupełnij
+	y = np.sin(x)*x							# tutaj uzupełnij
 	plt.figure()
 	plt.plot(x, y)
-	plt.savefig("../figures/twoj_numer_indeksu.png")
+	plt.savefig("../figures/346112.png")
 	return "Figure was succesfully saved"
 
 
