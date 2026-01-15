83ba0a76da6d9e0cbcfc3f47cc2f309be7bf9a69
ewelinakalicka <kalickaewelina@gmail.com>
2026-01-15 13:45:58 +0100

    changed the function and the name

--- pipeline/339665.py
+++ pipeline/339665.py
@@ -3,13 +3,14 @@ import numpy as np
 
 
 def plot_function():
-	x = np.linspace(-1, 1, 100)	# tutaj uzupełnij
-	y = np.sin(x)							# tutaj uzupełnij
+	x = np.linspace(-1, 1, 100)	            # tutaj uzupełnij
+	y = x**2 + 2*x							# tutaj uzupełnij
 	plt.figure()
 	plt.plot(x, y)
-	plt.savefig("../figures/twoj_numer_indeksu.png")
+	plt.savefig("../figures/339665.png")
 	return "Figure was succesfully saved"
 
 
 if __name__=="__main__":
 	print(plot_function())
+
