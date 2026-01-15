b398ee0490097fa9fe97b96f0427b1e9d782c3ae
rappmartaa <rapinska.martaa@gmail.com>
2026-01-15 13:42:29 +0100

    First commit

--- pipeline/324633.py
+++ pipeline/324633.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def plot_function():
-	x = np.linspace(-1, 1, 100)	# tutaj uzupełnij
-	y = np.sin(x)							# tutaj uzupełnij
+	x = np.linspace(-1, 1, 150)	# tutaj uzupełnij
+	y = np.cos(x)							# tutaj uzupełnij
 	plt.figure()
 	plt.plot(x, y)
-	plt.savefig("../figures/twoj_numer_indeksu.png")
+	plt.savefig("../figures/324633.png")
 	return "Figure was succesfully saved"
 
 
