9946fe0c331bce30ba83bf73eb174ec8ea9a4a56
Marcinryp <marlcin2004@gmail.com>
2026-01-15 14:01:55 +0100

    function

--- pipeline/346808.py
+++ pipeline/346808.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def plot_function():
 	x = np.linspace(-1, 1, 100)	# tutaj uzupełnij
-	y = np.sin(x)							# tutaj uzupełnij
+	y = np.sin(x**2 - 7*np.cos(x))							# tutaj uzupełnij
 	plt.figure()
 	plt.plot(x, y)
-	plt.savefig("../figures/twoj_numer_indeksu.png")
+	plt.savefig("../figures/346808.png")
 	return "Figure was succesfully saved"
 
 
